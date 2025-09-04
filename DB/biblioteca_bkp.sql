ff211b5ff96d66c0314d72c2e25c58d770281ec9
GUNNAR FORNAZIER GOMES <00001099296365sp@aluno.educacao.sp.gov.br>
2025-09-04 10:00:16 -0300

    q

--- DB/biblioteca_bkp.sql
+++ DB/biblioteca_bkp.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `alunos`
 --
-CREATE TABLE `biblioteca`;
+CREATE DATABASE `biblioteca`;
 DROP TABLE IF EXISTS `alunos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;

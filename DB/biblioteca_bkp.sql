b8324e064d2ab285b10213db61e79544e19d44c9
GUNNAR FORNAZIER GOMES <00001099296365sp@aluno.educacao.sp.gov.br>
2025-09-04 07:34:38 -0300

    w

--- DB/biblioteca_bkp.sql
+++ DB/biblioteca_bkp.sql
@@ -1,152 +1,143 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Tempo de geração: 27/02/2025 às 13:04
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version       8.0.43-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `biblioteca`
+-- Table structure for table `alunos`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `alunos`
---
-
+DROP TABLE IF EXISTS `alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alunos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(60) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `serie` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `serie` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `emprestimos`
+-- Dumping data for table `alunos`
 --
 
+LOCK TABLES `alunos` WRITE;
+/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
+INSERT INTO `alunos` VALUES (1,'Gunnar Fornazier Gomes','teste@gmail.com','3°D'),(2,'gunnar as','astostero@gmail.com','3°D');
+/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emprestimos`
+--
+
+DROP TABLE IF EXISTS `emprestimos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `emprestimos` (
-  `id` int(11) NOT NULL,
-  `id_aluno` int(11) NOT NULL,
-  `id_professor` int(11) NOT NULL,
-  `id_livro` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `aluno_id` int NOT NULL,
+  `professor_id` int NOT NULL,
+  `livro_id` int NOT NULL,
   `data_emprestimo` date NOT NULL,
-  `data_devolucao` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `data_devolucao` date NOT NULL,
+  `status` int DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_id_aluno` (`aluno_id`),
+  KEY `fk_id_professor` (`professor_id`),
+  KEY `fk_id_livro` (`livro_id`),
+  CONSTRAINT `fk_id_aluno` FOREIGN KEY (`aluno_id`) REFERENCES `alunos` (`id`),
+  CONSTRAINT `fk_id_livro` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`id`),
+  CONSTRAINT `fk_id_professor` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `livros`
+-- Dumping data for table `emprestimos`
 --
 
+LOCK TABLES `emprestimos` WRITE;
+/*!40000 ALTER TABLE `emprestimos` DISABLE KEYS */;
+INSERT INTO `emprestimos` VALUES (1,1,3,1,'2025-08-14','2025-08-22',0);
+/*!40000 ALTER TABLE `emprestimos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livros`
+--
+
+DROP TABLE IF EXISTS `livros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `livros` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `autor` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `autor` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `isbn` varchar(13) COLLATE utf8mb4_general_ci NOT NULL,
+  `data_publicacao` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `isbn` (`isbn`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `professores`
+-- Dumping data for table `livros`
 --
 
+LOCK TABLES `livros` WRITE;
+/*!40000 ALTER TABLE `livros` DISABLE KEYS */;
+INSERT INTO `livros` VALUES (1,'O Senhor dos Anéis: A Sociedade do Anel','J.R.R. Tolkien','9788595086333',NULL),(4,'Uma Breve História do Homem','Michael Cook, MARIA LUIZA X. DE A. BORGES','9788571108745',NULL),(5,'Intocável','Randall Sullivan','9788580868791',NULL),(6,'Uma breve história do tempo','Stephen Hawking','9788580576474','2015-01-13');
+/*!40000 ALTER TABLE `livros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professores`
+--
+
+DROP TABLE IF EXISTS `professores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `professores` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `senha_hash` varchar(258) NOT NULL,
-  `email` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `cpf` varchar(11) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha_hash` varchar(258) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `professores`
 --
 
---
--- Índices de tabela `alunos`
---
-ALTER TABLE `alunos`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `professores` WRITE;
+/*!40000 ALTER TABLE `professores` DISABLE KEYS */;
+INSERT INTO `professores` VALUES (1,'mano brown','11111111111','$2y$10$Z7Q4k2RmxpgGL1t5Oz4hi.6dzbnI/Erfm7d9OutDHz/BzwvCcqqmS','0'),(2,'frango','12345678911','$2y$10$Og7pW0nnl/r5Q6eS3IqDbuNTHhXzmG5sEl64gTw3/DfkQHscTUuWm','frango@sadia.com'),(3,'gunnar','57787703830','0c374bae70f14612bf14289924d8c36e','teste@gmail.com'),(4,'teste','00000000000','e10adc3949ba59abbe56e057f20f883e','teste@test.com');
+/*!40000 ALTER TABLE `professores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices de tabela `emprestimos`
---
-ALTER TABLE `emprestimos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_aluno` (`id_aluno`),
-  ADD KEY `fk_id_professor` (`id_professor`),
-  ADD KEY `fk_id_livro` (`id_livro`);
-
---
--- Índices de tabela `livros`
---
-ALTER TABLE `livros`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `professores`
---
-ALTER TABLE `professores`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `alunos`
---
-ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `emprestimos`
---
-ALTER TABLE `emprestimos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `livros`
---
-ALTER TABLE `livros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `professores`
---
-ALTER TABLE `professores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `emprestimos`
---
-ALTER TABLE `emprestimos`
-  ADD CONSTRAINT `fk_id_aluno` FOREIGN KEY (`id_aluno`) REFERENCES `alunos` (`id`),
-  ADD CONSTRAINT `fk_id_livro` FOREIGN KEY (`id_livro`) REFERENCES `livros` (`id`),
-  ADD CONSTRAINT `fk_id_professor` FOREIGN KEY (`id_professor`) REFERENCES `professores` (`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-04  7:33:04
